899cac9acec81f8541010a4f093f1c9166550138
Minusday <sky02106@nate.com>
2018-05-15 10:05:59 +0900

    Update controller.js

--- js/controller.js
+++ js/controller.js
@@ -59,14 +59,12 @@
             var tick = $interval(updateTime, 1000); // 1초 마다
             updateTime();
 
-            /** GPS 정보를 가져온다 */
             GeolocationService.getLocation({enableHighAccuracy: true}).then(function(geoposition){
                 console.log("Geoposition", geoposition);
                 $scope.map = MapService.generateMap(geoposition.coords.latitude+','+geoposition.coords.longitude);
             });
             restCommand();
 
-            /** 현재 장소를 가져오며, 날씨 정보를 가져온다. */
             var refreshMirrorData = function() {
                 //Get our location and then get the weather for our location
                 GeolocationService.getLocation({enableHighAccuracy: true}).then(function(geoposition){
@@ -83,21 +81,18 @@
                     console.log(error);
                 });
 
-                /** icals로 연동된 달력의 정보를 가져온다. */
                 CalendarService.getCalendarEvents().then(function(response) {
                     $scope.calendar = CalendarService.getFutureEvents();
                 }, function(error) {
                     console.log(error);
                 });
 
-                /** config.js의 greeting 배열(인사말의 정보)를 랜덤으로 가져온다 */
                 $scope.greeting = config.greeting[Math.floor(Math.random() * config.greeting.length)];
             };
 
             refreshMirrorData();
             $interval(refreshMirrorData, 3600000);
 
-            /** 출근에서 퇴근지 교통 정보 나오기*/
             var refreshTrafficData = function() {
                 TrafficService.getTravelDuration().then(function(durationTraffic) {
                     console.log("Traffic", durationTraffic);
@@ -117,71 +112,58 @@
             refreshTrafficData();
             $interval(refreshTrafficData, config.traffic.reload_interval * 60000);
 
-            /* Default뷰는 홈 화면*/
             var defaultView = function() {
             	functionService.defaultHome($scope);
             }
 
-            // 미러는 누구니
             AnnyangService.addCommand(command.whois,function() {
             	functionService.whoIsSmartMirror($scope);
             });
             
-            // 사용가능한 명령을 보여준다.
             AnnyangService.addCommand(command.whatcanisay, function() {
                functionService.whatCanISay($scope);
             });
 
-            // 홈화면으로
             AnnyangService.addCommand(command.home, defaultView);
-
-            // 미러의 화면을 끈다.
+		
             AnnyangService.addCommand(command.sleep, function() {
             	functionService.goSleep($scope);
             });
 
-            // 미러의 화면을 켠다.
             AnnyangService.addCommand(command.wake, function() {
             	functionService.wake($scope);
             });
 
-            // 디버그의 정보를 보여준다.
             AnnyangService.addCommand(command.debug, function() {
                 console.debug("Boop Boop. Showing debug info...");
                 $scope.debug = true;
             });
 
-            // 현재 위치의 지도를 보여준다.
             AnnyangService.addCommand(command.map, function() {
                 functionService.map($scope,GeolocationService,MapService);
              });
 
-            // 특정 위치의 지도를 보여준다.
             AnnyangService.addCommand(command.locaiton, function(location) {
             	console.debug("Getting map of", location);
                 $scope.map = MapService.generateMap(location);
                 $scope.focus = "map";
             });
 
-            // 지도를 확대한다.
             AnnyangService.addCommand(command.zoomin, function() {
                 console.debug("Zoooooooom!!!");
                 $scope.map = MapService.zoomIn();
             });
 
-            // 지도를 축소한다.
             AnnyangService.addCommand(command.zoomout, function() {
                 console.debug("Moooooooooz!!!");
                 $scope.map = MapService.zoomOut();
             });
 
-            // 지도의 줌 값을 정한다. 
             AnnyangService.addCommand(command.zoomvalue, function(value) {
                 console.debug("Moooop!!!", value);
                 $scope.map = MapService.zoomTo(value);
             });
 
-            // 지도의 줌 값을 리셋 한다.
             AnnyangService.addCommand(command.zoomreset, function() {
                 console.debug("Zoooommmmmzzz00000!!!");
                 $scope.map = MapService.reset();
@@ -189,8 +171,6 @@
             });
 
 
-            /** Youtube API */
-            // Youtube 동영상 재생 
             AnnyangService.addCommand(command.playyoutube, function(term) {
               YoutubeService.getYoutube(term,'video').then(function(){
                 if(term){
@@ -202,7 +182,6 @@
               });
             });
 
-            // Youtube 플레이어 리스트 재생
             AnnyangService.addCommand(command.ytbplaylist, function(term) {
               YoutubeService.getYoutube(term,'playlist').then(function(){
                 if(term){
@@ -214,15 +193,12 @@
               });
             });
 
-            // Youtube 동영상, 플레이어 리스트 정지
             AnnyangService.addCommand(command.stopyoutube, function() {
               var iframe = document.getElementsByTagName("iframe")[0].contentWindow;
               iframe.postMessage('{"event":"command","func":"' + 'stopVideo' +   '","args":""}', '*');
               $scope.focus = "default";
             });
 
-            /** Subway */
-            // 지하철 도착 정보 
             AnnyangService.addCommand(command.subway, function(station,linenumber,updown) {
               SubwayService.init(station).then(function(){
                 SubwayService.getArriveTime(linenumber,updown).then(function(data){
@@ -243,56 +219,35 @@
               });
             });
             
-            /** Google News */
-            // 구글 뉴스를 보여준다.
             AnnyangService.addCommand(command.news, function() {
             	functionService.news($scope);
             });
-            /** Raspberry Camera */
-            // 라즈베리 카메라를 이용해 사진 촬영
+
             AnnyangService.addCommand(command.photo, function() {
             	functionService.photo(PHOTO_INDEX);
             	PHOTO_INDEX++;
             });
 
-            // 동영상 촬영
             AnnyangService.addCommand(command.video, function() {
             	functionService.video(VIDEO_INDEX);
         		VIDEO_INDEX++;
             });
             
-            /** Relay Switch control Light */
-            /* npm install onoff */
-            // 릴레이 스위치 ON -> Light on
-            AnnyangService.addCommand(command.lighton,function(state,action) {
-            	functionService.lightOn();
-            });
-            
-            // 릴레이 스위치 OFF -> Light off
-            AnnyangService.addCommand(command.lightoff,function() {
-            	functionService.lightOff();
-            });
-            
-            /** Sound Cloud */
-            /*
-            // 음악 재생
             AnnyangService.addCommand(command.musicplay,function(state,action) {
             	console.log("음악 시작");
             	$scope.musicplay.play(); // 음악 시작
             	
             });
             
-            // 음악정지
             AnnyangService.addCommand(command.musicplay,function(state,action) {
             	console.log("음악 정지");
             	$scope.musicplay.pause(); // 음악 정지
             });
             */
         
-            /** 안드로이드에서 보낸 SST 명령어를 미러와 동작하게 하는 부분*/
             var sender = require('remote').getGlobal('sender');
      	    sender.on('android',function(android){
-     	    	$scope.interimResult = android.command; // 미러의 음성인식된 문구에 보여짐
+     	    	$scope.interimResult = android.command; 
 	    		console.log("Android Command :: "+android.command);
 	    		var androidCommand = android.command+"";
 	    		
@@ -308,7 +263,6 @@
     			else if(androidCommand === command.lighton) { functionService.lightOn();}
     			else if(androidCommand === command.lightoff) { functionService.lightOff();}
     			
-    			/* Map Service ***의 위치 보여줘 */
     			var locationExist = androidCommand.indexOf("위치");
 	    		if(locationExist != -1) {
 	    			var locationValue = androidCommand.split("위치");
@@ -316,7 +270,6 @@
 	    			functionService.location(locationValue[0],$scope,GeolocationService,MapService);
 	    		}
 	    		
-	    		/* Youtube *** 동영상 보여줘 */
 	    		var youtubeExist = androidCommand.indexOf("동영상");
 	    		if(youtubeExist != -1) {
 	    			if(androidCommand === "동영상 정지") {
@@ -328,10 +281,8 @@
 	    			}
      	    	}
 	    		
-	    		/* 지하철 **역 *호선 *행성 */
 	    		var subwayExist = androidCommand.indexOf("역");
 	    		if(subwayExist != -1) {
-	    			// OO역 OO호선 상(하)행선
 	    			var temp1 = androidCommand.split("역");
 	    			var temp2 = temp1[1].split("호선");
 	    			
@@ -347,7 +298,6 @@
 	    
 
             var resetCommandTimeout;
-            //Track when the Annyang is listening to us
             AnnyangService.start(function(listening){
                 $scope.listening = listening;
             }, function(interimResult){
@@ -358,7 +308,7 @@
                 resetCommandTimeout = $timeout(restCommand, 5000);
             });
             
-            $scope.interimResult = DEFAULT_COMMAND_TEXT; // 미러의 음성인식된 문구에 보여짐
+            $scope.interimResult = DEFAULT_COMMAND_TEXT;
         };
 
         _this.init();
